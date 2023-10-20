051bcd084e426e0a2752878e2c529ab0bbaf61de
Bokayy X <blabsy8@gmail.com>
2023-10-20 18:23:33 +0200

    i don't know

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ import scriptSetup from 'unplugin-vue2-script-setup/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 9000
+  },
   plugins: [
     vue2({
       jsx: true,
