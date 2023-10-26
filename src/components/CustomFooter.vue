a4884217731eeaca2a886437a9c5b3dbae5a98b4
Bokayy X <blabsy8@gmail.com>
2023-10-26 06:07:50 +0200

    1.) added column gap to items 2.) removed experimental page svg's

--- src/components/CustomFooter.vue
+++ src/components/CustomFooter.vue
@@ -1,20 +1,8 @@
 <template>
     <header>
         <div class="break">&nbsp;</div>
-        <div class = "pageButtons" v-for="index in maxPages" :key="index">
-            <svg height="100" width="100">
-                <circle cx="50" cy="50" r="8" stroke="rgb(197, 164, 120)" stroke-width="3" fill="white"/>
-            </svg>
-        </div>
-        
-        <!-- <div v-show="showPagination" class="pageIndicator">
-            <i @click="pageminus" class="fa-solid fa-angle-left" id="btnLeft"></i>
-            <p class="pageNo">Page:{{pageNo}}/{{maxPages}}</p>
-            <i @click="pageplus" class="fa-solid fa-angle-right" id="btnRight"></i>
-        </div> -->
     </header>
 
-    
 </template>
 
 <script>
